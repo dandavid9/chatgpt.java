771722a9b68f4465987d49f72a7fb1aa2b1de8b0
Bruno Jesus <bruno.fl.jesus@gmail.com>
2023-03-29 15:21:38 +0100

    Add FileUtils

--- src/main/java/pt/brunojesus/chatgpt/Main.java
+++ src/main/java/pt/brunojesus/chatgpt/Main.java
@@ -1,13 +1,27 @@
 package pt.brunojesus.chatgpt;
 
+import java.io.IOException;
+import java.util.List;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import pt.brunojesus.chatgpt.model.request.*;
+import pt.brunojesus.chatgpt.util.FileUtils;
 
 public class Main {
 	
 	public static void main(String[] args) {
+		FileUtils fileUtils = new FileUtils();
+		List<String> lines = null;
+		try {
+			lines = fileUtils.readLines("application.properties");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(lines);
+		
 		
 		OpenAiRequest req = new OpenAiRequest();
 		req.setModel("text-davinci-003");
