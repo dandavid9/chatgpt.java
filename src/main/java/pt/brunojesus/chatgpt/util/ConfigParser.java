f2b383815ce75e0c0e151cf9e8eec757c6ea051c
Bruno Jesus <bruno.fl.jesus@gmail.com>
2023-04-12 15:26:11 +0100

    Add unit tests for ConfigParser

--- src/main/java/pt/brunojesus/chatgpt/util/ConfigParser.java
+++ src/main/java/pt/brunojesus/chatgpt/util/ConfigParser.java
@@ -10,25 +10,28 @@ public class ConfigParser {
 	// Map<Tipo_De_Dados_Chave, Tipo_De_Dados_Valor>
 	public static Map<String, String> parse(String filePath) throws IOException {
 		Map<String, String> result = new HashMap<>();
-				
+
 		List<String> lines = FileUtils.readLines(filePath);
 		// KEY=VALUE
-		
-		for (int i=0; i < lines.size(); i++) {
-			String line = lines.get(i); //equivalente a lines[i] num array
-			
-			int splitPosition = line.indexOf("=");
-			
-			// key sera tudo o que está à esquerda do primeiro = (splitPosition)
-			String key = line.substring(0, splitPosition);
-			
-			// value sera tudo o que está à direita do primeiro = (splitPosition)
-			String value = line.substring(splitPosition + 1);
-			
-			result.put(key, value);
+
+		for (int i = 0; i < lines.size(); i++) {
+			String line = lines.get(i).trim(); // equivalente a lines[i] num array
+
+			if (line.startsWith("#") == false) {
+				int splitPosition = line.indexOf("=");
+
+				if (splitPosition != -1) {
+					// key sera tudo o que está à esquerda do primeiro = (splitPosition)
+					String key = line.substring(0, splitPosition);
+
+					// value sera tudo o que está à direita do primeiro = (splitPosition)
+					String value = line.substring(splitPosition + 1);
+
+					result.put(key, value);
+				}
+			}
 		}
-		
-		
+
 		return result;
 	}
 }
