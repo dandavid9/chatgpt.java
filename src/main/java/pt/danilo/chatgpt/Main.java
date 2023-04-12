9de6d8e813688b3d404d9e9409363a8432b4eacc
alunoESGTS <alunoESGTS@SI2_08.ESG.IPSantarem.local>
2023-04-12 15:34:23 +0100

    Add unit tests for ConfigParser

--- src/main/java/pt/danilo/chatgpt/Main.java
+++ src/main/java/pt/danilo/chatgpt/Main.java
@@ -1,14 +1,24 @@
 package pt.danilo.chatgpt;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import pt.danilo.chatgpt.model.request.*;
+import pt.danilo.chatgpt.util.ConfigParser;
+import pt.danilo.chatgpt.util.FileUtils;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	public static void main(String[] args) throws IOException {
+		Map<String, String> config = ConfigParser.parse("application.properties");
+		String greeting = config.get("greeting");
+		
+		System.out.println(greeting);
+		
 		OpenAiRequest req = new OpenAiRequest();
 		req.setModel("text-davinci-003");
 		req.setPrompt("You are and AI\nAI:");
@@ -17,17 +27,17 @@ public class Main {
 		req.setTopP(1.0f);
 		req.setFrequencyPenalty(0.5f);
 		req.setPresencePenalty(0.5f);
-		req.setStop(new String[] {"AI: "});
-		
+		req.setStop(new String[] {"AI:"});
+				
 		ObjectMapper mapper = new ObjectMapper();
 		String jsonPayload = null;
 		try {
 			jsonPayload = mapper.writeValueAsString(req);
 		} catch (JsonProcessingException e) {
-			// TODO: handle exception
 			System.err.println(e.getMessage());
 			return;
 		}
+		
 		System.out.println(jsonPayload);
 	}
 
